0f104ca1eb249e4ca9e542fb957363ac8fca36a9
Aayush Maurya <aayushmaurya1099@gmail.com>
2022-02-13 17:49:56 +0530

    overview for client

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,9 @@ import { adminLogout } from "./redux/actions/adminAction";
 import Poll from "./pages/Poll";
 import AdminSignup from "./pages/admin/AdminSignup";
 import CreatePoll from "./pages/poll/CreatePoll";
+import ClientLogin from "./pages/client/ClientLogin";
+import ClientSignup from "./pages/client/ClientSignup";
+import ClientHome from "./pages/client/ClientHome";
 
 if (window.localStorage.adminJwtToken) {
 
@@ -41,6 +44,9 @@ function App() {
           <Route exact path='/adminLogin' element={<AdminLogin />} />
           <Route exact path='/admin' element={<AdminHome />} />
           <Route exact path='/adminSignup' element={<AdminSignup />} />
+          <Route exact path='/clientLogin' element={<ClientLogin />} />
+          <Route exact path='/clientSignup' element={<ClientSignup />} />
+          <Route exact path='/client' element={<ClientHome />} />
           <Route exact path='/createPoll' element={<CreatePoll />} />
           <Route path="/poll/:id" element = {<Poll />} />
         </Routes>
