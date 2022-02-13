fddf20ce52768d797007c8d27c37c54fe535f642
Aayush Maurya <aayushmaurya1099@gmail.com>
2022-02-13 17:22:33 +0530

    first commit

---
+++ client/src/pages/authorizedUser/AuthorizedUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthorizedUser = () => {
+    return(
+        <>
+            <h1>Hello! this is home page for Authorized user</h1>
+        </>
+    )
+}
+
+export default AuthorizedUser;
