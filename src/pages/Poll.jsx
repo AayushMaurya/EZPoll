4345ec85403e031a58eda509216c883abe079904
Gaurav Gupta <gaurav1122gupta@gmail.com>
2022-03-25 21:53:01 +0530

    gaurav edit

--- src/pages/Poll.jsx
+++ src/pages/Poll.jsx
@@ -4,91 +4,115 @@ import { getPollInfo, submitPollChoice } from "../apis/pollApi";
 import { useNavigate } from "react-router";
 
 const Poll = () => {
-    const { id } = useParams();
-    const [pollinfo, setPollinfo] = useState({});
-    const [isPoll, setIsPoll] = useState(false);
-    const navigate = useNavigate();
-    const [userChoice, setUserChoice] = useState({
-        // poll_id: id, ye backup hai
-        _id: "",
-        choice: ""
+  const { id } = useParams();
+  const [pollinfo, setPollinfo] = useState({});
+  const [isPoll, setIsPoll] = useState(false);
+  const navigate = useNavigate();
+  const [userChoice, setUserChoice] = useState({
+    // poll_id: id, ye backup hai
+    _id: "",
+    choice: "",
+  });
+  const [loadinSubmit, setLoadingSubmit] = useState(false);
+
+  useEffect(async () => {
+    const data = await getPollInfo(id);
+    console.log(data);
+    if (data.success) {
+      setPollinfo(data.result[0]);
+      // console.log("pollinfo: ", pollinfo);
+      setIsPoll(true);
+      setUserChoice({
+        ...userChoice,
+        poll_id: id,
+        _id: pollinfo._id,
+      });
+    } else {
+      alert(data.message);
+      navigate("/polls");
+    }
+  }, []);
+
+  const changeHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUserChoice({
+      ...userChoice,
+      [name]: value,
     });
-    const [loadinSubmit, setLoadingSubmit] = useState(false);
+  };
 
-    useEffect(async () => {
-        const data = await getPollInfo(id);
-        console.log(data);
-        if (data.success) {
-            setPollinfo(data.result[0]);
-            // console.log("pollinfo: ", pollinfo);
-            setIsPoll(true);
-            setUserChoice({
-                ...userChoice,
-                poll_id: id,
-                _id: pollinfo._id
-            });
-        }
-        else {
-            alert(data.message);
-            navigate('/polls');
-        }
-    }, []);
+  const formHandler = async (e) => {
+    e.preventDefault();
+    setLoadingSubmit(true);
 
-    const changeHandler = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
+    console.log("pollinfo: ", pollinfo);
+    console.log("usechoice: ", userChoice);
 
-        setUserChoice({
-            ...userChoice,
-            [name]: value
-        });
+    const data = await submitPollChoice(userChoice);
+    if (data.success) {
+      alert("choice successfully submited");
+      navigate(`/pollResult/${id}`);
+    } else {
+      alert(data.message);
+      setLoadingSubmit(false);
+      if (data.message == "Already voted") navigate(`/pollResult/${id}`);
     }
+  };
 
-    const formHandler = async (e) => {
-        e.preventDefault();
-        setLoadingSubmit(true);
-
-        console.log("pollinfo: ", pollinfo);
-        console.log("usechoice: ", userChoice);
-
-        const data = await submitPollChoice(userChoice);
-        if(data.success)
-        {
-            alert("choice successfully submited");
-            navigate(`/pollResult/${id}`);
-        }
-        else{
-            alert(data.message);
-            setLoadingSubmit(false);
-            if(data.message == "Already voted")
-                navigate(`/pollResult/${id}`);
-            
-        }
-    }
-
-    return (
-        <>
-            {!isPoll && <h1> Loading poll {id} ...</h1>}
-            {isPoll && <div>
-                <h1>{pollinfo.title}</h1>
-                <h3>{pollinfo.description}</h3>
-                <h3>Choices: </h3>
-                <br />
-                <form onSubmit={formHandler}>
-                    <label>{pollinfo.choice1}</label>
-                    <input type="radio" name="choice" value="choice1" onChange={changeHandler} />
-                    <br />
-                    <label>{pollinfo.choice2}</label>
-                    <input type="radio" name="choice" value="choice2" onChange={changeHandler} />
-                    <br />
-                    <label>{pollinfo.choice3}</label>
-                    <input type="radio" name="choice" value="choice3" onChange={changeHandler} />
-                    <br />
-                    {!loadinSubmit && <button type="submit" >Submit</button>}
-                </form>
-            </div>}
-        </>
-    )
-}
+  return (
+    <>
+      {!isPoll && <h1> Loading poll {id} ...</h1>}
+      {isPoll && (
+        <div>
+          <div className="container pollSec">
+            <div className="row pollHeader">
+              <h1 className="pollTitle">{pollinfo.title}</h1>
+              <h3 className="pollDes">{pollinfo.description}</h3>
+            </div>
+            <div className="row pollFooter">
+              <h3 className="pollChoices">Choices </h3>
+              <form onSubmit={formHandler}>
+                <div className="row">
+                  <input
+                    type="radio"
+                    name="choice"
+                    value="choice1"
+                    id="choice1"
+                    className="choice"
+                    onChange={changeHandler}
+                    />
+                    <label for = "choice1">{pollinfo.choice1}</label> <br />
+                  <input
+                    type="radio"
+                    name="choice"
+                    value="choice2"
+                    id="choice2"
+                    className="choice"
+                    onChange={changeHandler}
+                    />
+                    <label for = "choice2">{pollinfo.choice2}</label> <br />
+                  <input
+                    type="radio"
+                    name="choice"
+                    value="choice3"
+                    id="choice3"
+                    className="choice"
+                    onChange={changeHandler}
+                    />
+                    <label for = "choice3">{pollinfo.choice3}</label> <br />
+                </div>
+                <div className="row pollsubbtn">
+                  {!loadinSubmit && <button type="submit" className="pollsubmitBtn">Submit</button>}
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Poll;
--- src/pages/poll/CreatePoll.jsx
+++ src/pages/poll/CreatePoll.jsx
@@ -4,108 +4,162 @@ import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
 
 const CreatePoll = () => {
+  const [pollinfo, setPollInfo] = useState({
+    title: "",
+    description: "",
+    choice1: "",
+    choice2: "",
+    choice3: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isCreated, setIsCreated] = useState(false);
+  const [poll_id, setpoll_id] = useState("");
+  const navigate = useNavigate();
+  const store = useSelector((store) => store);
 
-    const [pollinfo, setPollInfo] = useState({
-        title: "",
-        description: "",
-        choice1: "",
-        choice2: "",
-        choice3: ""
+  useEffect(() => {
+    // iski wajah se back button press karne par wapas nhi ja pa rhe
+    // agar login nhi hai to loop me ho ja rha hai
+    if (!store.client.isAuthenticated) navigate("/clientSignup");
+  }, []);
+
+  const changeHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setPollInfo({
+      ...pollinfo,
+      [name]: value,
     });
-    const [isLoading, setIsLoading] = useState(false);
-    const [isCreated, setIsCreated] = useState(false);
-    const [poll_id, setpoll_id] = useState("");
-    const navigate = useNavigate();
-    const store= useSelector((store) => store);
+  };
 
-    useEffect(() => {
-        // iski wajah se back button press karne par wapas nhi ja pa rhe
-        // agar login nhi hai to loop me ho ja rha hai
-        if(!store.client.isAuthenticated)
-            navigate('/clientSignup');
-    }, []);
+  const formHandler = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-    const changeHandler = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
+    var data = await createPoll(pollinfo);
 
-        setPollInfo({
-            ...pollinfo,
-            [name]: value
-        });
+    if (data.success) {
+      alert("Poll successfully created");
+      setIsCreated(true);
+      setpoll_id(data.poll.poll_id);
+    } else {
+      setIsLoading(false);
+      alert(data.message);
     }
+  };
 
-    const formHandler = async (e) => {
-        e.preventDefault();
-        setIsLoading(true);
-
-        var data = await createPoll(pollinfo);
-
-        if (data.success) {
-            alert("Poll successfully created");
-            setIsCreated(true);
-            setpoll_id(data.poll.poll_id);
-        }
-        else{
-            setIsLoading(false);
-            alert(data.message);
-        }
-    }
-
-    return (
-        <>
-            {!isCreated && <div>
-                <h1>Create a poll</h1>
-                <form onSubmit={formHandler}>
-                    <lable>Title: </lable>
-                    <input type="text" name="title" required value={pollinfo.title} onChange={changeHandler} />
-                    <br />
-                    <lable>Description: </lable>
-                    <input type="text" name="description" value={pollinfo.desc} onChange={changeHandler} />
-                    <br />
-                    <lable>Answer options: </lable>
-                    <input type="text" name="choice1" required value={pollinfo.choice1} onChange={changeHandler} />
-                    <br />
-                    <input type="text" name="choice2" required value={pollinfo.choice2} onChange={changeHandler} />
-                    <br />
-                    <input type="text" name="choice3" required value={pollinfo.choice3} onChange={changeHandler} />
-                    <br />
-                    {!isLoading && <button type="submit">Create</button>}
-                </form>
-            </div>}
-            {isCreated && <div>
-                <h1>Your poll is successfull created</h1>
-                <div><table className="table border">
-                        <tbody>
-                            <tr>
-                                <td>Title</td>
-                                <td>{pollinfo.title}</td>
-                            </tr>
-                            <tr>
-                                <td>Desc</td>
-                                <td>{pollinfo.description}</td>
-                            </tr>
-                            <tr>
-                                <td>choice1</td>
-                                <td>{pollinfo.choice1}</td>
-                            </tr>
-                            <tr>
-                                <td>choice2</td>
-                                <td>{pollinfo.choice2}</td>
-                            </tr>
-                            <tr>
-                                <td>choice3</td>
-                                <td>{pollinfo.choice3}</td>
-                            </tr>
-                            <tr>
-                                <td>share</td>
-                                <td>http://localhost:3000/poll/{poll_id}</td>
-                            </tr>
-                        </tbody>
-                    </table></div>
-            </div>}
-        </>
-    );
-}
+  return (
+    <>
+      {!isCreated && (
+        <div>
+          <div className="container createForm">
+            <div className="row formHead">
+              <div className="row">
+                <h3 className="crTitle">Create a Poll</h3>
+              </div>
+              <div className="row">
+                <h4 className="crSubtitle">
+                  Fill the below form to create your Poll
+                </h4>
+              </div>
+            </div>
+            <div className="row crForm">
+              <form onSubmit={formHandler}>
+                <h4 className="formEle">Title</h4>
+                <input
+                  type="text"
+                  className="formInp"
+                  name="title"
+                  required
+                  value={pollinfo.title}
+                  onChange={changeHandler}
+                  
+                />
+                <br />
+                <h4 className="formEle">Description</h4>
+                <input
+                  type="text"
+                  className="formInp"
+                  name="description"
+                  value={pollinfo.desc}
+                  onChange={changeHandler}
+                />
+                <br />
+                <h4 className="formEle">Answer options </h4>
+                <input
+                  type="text"
+                  className="formInp"
+                  name="choice1"
+                  required
+                  value={pollinfo.choice1}
+                  onChange={changeHandler}
+                  placeholder = "Option 1 "
+                />
+                <input
+                  type="text"
+                  className="formInp"
+                  name="choice2"
+                  required
+                  value={pollinfo.choice2}
+                  onChange={changeHandler}
+                  placeholder = "Option 2 "
+                />
+                <input
+                  type="text"
+                  className="formInp"
+                  name="choice3"
+                  required
+                  value={pollinfo.choice3}
+                  onChange={changeHandler}
+                  placeholder = "Option 3 "
+                />
+                <div className="row crBtn">
+                    
+                {!isLoading && <button type="submit" className="createbtn">Create</button>}
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+      {isCreated && (
+        <div>
+          <h1>Your poll is successfull created</h1>
+          <div>
+            <table className="table border">
+              <tbody>
+                <tr>
+                  <td>Title</td>
+                  <td>{pollinfo.title}</td>
+                </tr>
+                <tr>
+                  <td>Desc</td>
+                  <td>{pollinfo.description}</td>
+                </tr>
+                <tr>
+                  <td>choice1</td>
+                  <td>{pollinfo.choice1}</td>
+                </tr>
+                <tr>
+                  <td>choice2</td>
+                  <td>{pollinfo.choice2}</td>
+                </tr>
+                <tr>
+                  <td>choice3</td>
+                  <td>{pollinfo.choice3}</td>
+                </tr>
+                <tr>
+                  <td>share</td>
+                  <td>http://localhost:3000/poll/{poll_id}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default CreatePoll;
